bbdee576a8046c06530a74601209d13fff8bedaa
JolyneKo <jolynek4444@protonmail.com>
2021-02-26 18:47:15 -0400

    Adicionado Navbar pra mobile
    Adicionado Navbar pra mobile e menu

--- app/src/Components/Menu.js
+++ app/src/Components/Menu.js
@@ -1,6 +1,28 @@
+import '../SCSS/menu.scss';
+
 function Menu() {
   return (
-    <div className="menu"></div>
+    <div className="menu">
+      <h2 className="menu_header">Menu</h2>
+
+      <ul className="menu_links">
+        <li className="link">
+          <a href="#.">Home</a>
+        </li>
+        <li className="link">
+          <a href="#.">News</a>
+        </li>
+        <li className="link">
+          <a href="#.">Terms</a>
+        </li>
+        <li className="link">
+          <a href="#.">About Us</a>
+        </li>
+        <li className="link">
+          <a href="#.">Contact</a>
+        </li>
+      </ul> 
+    </div>
   )
 }
 
